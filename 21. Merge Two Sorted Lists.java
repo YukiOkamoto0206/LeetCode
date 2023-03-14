775245c50a25bb984222df7c5c00d9b8b05be6b1
Yuki Okamoto <75482698+YukiOkamoto0206@users.noreply.github.com>
2023-03-13 22:58:42 -0700

    Create 21. Merge Two Sorted Lists.java

---
+++ 21. Merge Two Sorted Lists.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode list = new ListNode(0);
+        ListNode answer = list;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list.next = list1;
+                list1 = list1.next;
+            } else {
+                list.next = list2;
+                list2 = list2.next;
+            }
+            list = list.next;
+        }
+
+        if (list1 != null) {
+            list.next = list1;
+        }
+        if (list2 != null) {
+            list.next = list2;
+        }
+        return answer.next;
+    }
+}
