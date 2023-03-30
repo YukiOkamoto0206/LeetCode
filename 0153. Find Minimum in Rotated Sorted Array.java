3df701618af1c9f1e05b67fc99126ff5c15eea41
Yuki Okamoto <75482698+YukiOkamoto0206@users.noreply.github.com>
2023-03-29 17:58:42 -0700

    Update 0153. Find Minimum in Rotated Sorted Array.java

--- 0153. Find Minimum in Rotated Sorted Array.java
+++ 0153. Find Minimum in Rotated Sorted Array.java
@@ -1,3 +1,34 @@
+class BetterSolution {
+    public int findMin(int[] nums) {
+      // 3,4,5,1,2
+      //       l 
+      //       m   
+      //     r  
+
+      // 4,5,6,7,0,1,2
+      //         l m r
+      //         r   
+      //             
+
+      // 3,1,2
+      //   l     
+      // m r           
+      //              
+      int l = 0, r = nums.length-1;
+      while (l < r) {
+        int m = l + (r - l) / 2;
+        if (nums[r] < nums[m]) {
+          l = m + 1;
+        } else {
+          r = m;
+        }
+      }
+      return nums[l];
+    }
+}
+
+
+
 class Solution {
     public int findMin(int[] nums) {
         int l = 0;
